a8539b9dd9dea6e971219bf50cbe4ae13fc921b6
Rich Ross <rich.ross@live.com>
2023-05-25 15:41:32 -0400

    Initial Checkin

---
+++ RoomMonitor/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RoomMonitor;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ RoomMonitor/Models/Weather.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomMonitor.Models
+{
+    internal class Weather
+    {
+        public int id { get; set; }
+        public DateTime Date { get; set; }
+        public string DayOfWeek { get; set; }
+        public string Outlook { get; set; }
+        public float Temperature { get; set; }
+    }
+}
